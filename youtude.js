42b75a7277ec8a7cbedd2db37282168d8e369277
Ahmed Yasser Singap <130315264+singap88a@users.noreply.github.com>
2024-07-05 08:25:20 -0700

    Add files via upload

---
+++ youtude.js
@@ -0,0 +1,33 @@
+ 
+
+
+
+
+let loginForm = document.getElementById('open');
+ 
+ 
+ 
+function showform(){
+    loginForm.classList.add("active")
+  
+
+ 
+}
+function hideform(){
+    loginForm.classList.remove("active")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
